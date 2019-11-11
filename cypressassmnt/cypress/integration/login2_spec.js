32c0ad4f4fb3acaa6addad5e679594deb5bacd4c
Harsha Varthini <45447404+Harshavarthinisankar@users.noreply.github.com>
2019-11-11 12:20:21 +0530

    Update login2_spec.js

--- cypressassmnt/cypress/integration/login2_spec.js
+++ cypressassmnt/cypress/integration/login2_spec.js
@@ -1,11 +1,17 @@
 describe('Login-In Testing wesite', () =>  {
     it('Login scenario', () => {
+        //Visiting the website
         cy.visit('http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx')
+        //user name
         cy.get('div.login > input:nth-of-type(1)').type('Tester')
+        //password
         cy.get('div.login > input:nth-of-type(2)').type('test')
+        //submitting the form
         cy.get('input.button').click()
         cy.get('ul.menu > li:nth-of-type(1) > a').click()
+        //selecting the record
         cy.get('tbody > tr:nth-of-type(2) > td:nth-of-type(1) > input').click()
+        //delevting the record
         cy.get('input.btnDeleteSelected').click()
         cy.get('ul.menu > li:nth-of-type(3) > a').click()
         //Order testing-Dropdown box 
@@ -13,6 +19,7 @@ describe('Login-In Testing wesite', () =>  {
         cy.get('td > ol:nth-of-type(1) > li:nth-of-type(2) > input').type('10')
         cy.get('td > ol:nth-of-type(1) > li:nth-of-type(3) > input').type('75')
         cy.get('td > ol:nth-of-type(1) > li:nth-of-type(4) > input').type('30')
+        //calculating 
         cy.get('td > ol:nth-of-type(1) > li:nth-of-type(5) > input.btn_dark').click()
         //address info
         cy.get('td > ol:nth-of-type(2) > li:nth-of-type(1) > input').type('abced')
@@ -26,6 +33,7 @@ describe('Login-In Testing wesite', () =>  {
         cy.get('tr > td:nth-of-type(1) > label').click()
         cy.get('td > ol:nth-of-type(3) > li:nth-of-type(2) > input').type('098765432112')
         cy.get('td > ol:nth-of-type(3) > li:nth-of-type(3) > input').type('25/11')
+        //submiting the card details And proceeding
         cy.get('a.btn_light').click()
 
         //Back to first page
