f0d6087b6c5cf79679b888a78e967c44a7be943d
Harsha Varthini <45447404+Harshavarthinisankar@users.noreply.github.com>
2019-11-11 12:13:34 +0530

    Update login_spec.js

--- cypressassmnt/cypress/integration/login_spec.js
+++ cypressassmnt/cypress/integration/login_spec.js
@@ -1,10 +1,15 @@
 context('Login-IRCTC', () =>  {
     it('Login scenario', () => {
         cy.visit('https://www.irctc.co.in/nget/train-search')
+        //for clicking the menu button
         cy.get('.h_menu_drop_button.hidden-xs > a > .fa').click()
+        //clcking the login button
         cy.get('.ng-star-inserted > .search_btn').click()
-        cy.get('#userId').type('harshavarthinisankar@gmail.com')
+        //inserting the username
+        cy.get('#userId').type('xyzabs@gmail.com')
+        //inserting the password
         cy.get('#pwd').type('Sh722ixbc')
+        //clicking the submit button
         cy.get('form > button.search_btn').click()
         cy.title().should('include','My account')
         
